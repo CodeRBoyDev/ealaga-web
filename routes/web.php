3fd877661053b192692abc98d03504eb098bf3cd
Russel Solleza <sollezapuyo.russel@gmail.com>
2023-08-18 10:49:02 +0800

    Middleware configured, 404 added

--- routes/web.php
+++ routes/web.php
@@ -49,24 +49,23 @@ Route::get('/ticket', function () {
 })->name('ticket');
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
-
+Route::middleware(['access_level:2'])->group(function () {
 // Client
-
-Route::get('/client/home', [ClientHomeController::class, 'ClientHome'])->name('ClientHome');
-Route::get('/client/schedule', [ScheduleController::class, 'ClientSchedule'])->name('ClientSchedule');
-Route::post('/client/schedule/add', [ScheduleController::class, 'ClientScheduleAdd'])->name('ClientScheduleAdd');
-Route::get('/client/schedule/slot', [ScheduleController::class, 'ClientScheduleSlot'])->name('ClientScheduleSlot');
-Route::get('/client/schedule/list', [ScheduleController::class, 'ClientScheduleList'])->name('ClientScheduleList');
-Route::get('/client/schedule/list/{id}', [ScheduleController::class, 'ClientScheduleView'])->name('ClientScheduleView');
-Route::get('/client/schedule/history', [ScheduleController::class, 'ClientScheduleHistory'])->name('ClientScheduleHistory');
-Route::get('/client/schedule/cancel/{id}', [ScheduleController::class, 'ClientScheduleCancel'])->name('ClientScheduleCancel');
-Route::post('/client/review/add', [ScheduleController::class, 'ClientScheduleReview'])->name('ClientScheduleReview');
+    Route::get('/client/home', [ClientHomeController::class, 'ClientHome'])->name('ClientHome');
+    Route::get('/client/schedule', [ScheduleController::class, 'ClientSchedule'])->name('ClientSchedule');
+    Route::post('/client/schedule/add', [ScheduleController::class, 'ClientScheduleAdd'])->name('ClientScheduleAdd');
+    Route::get('/client/schedule/slot', [ScheduleController::class, 'ClientScheduleSlot'])->name('ClientScheduleSlot');
+    Route::get('/client/schedule/list', [ScheduleController::class, 'ClientScheduleList'])->name('ClientScheduleList');
+    Route::get('/client/schedule/list/{id}', [ScheduleController::class, 'ClientScheduleView'])->name('ClientScheduleView');
+    Route::get('/client/schedule/history', [ScheduleController::class, 'ClientScheduleHistory'])->name('ClientScheduleHistory');
+    Route::get('/client/schedule/cancel/{id}', [ScheduleController::class, 'ClientScheduleCancel'])->name('ClientScheduleCancel');
+    Route::post('/client/review/add', [ScheduleController::class, 'ClientScheduleReview'])->name('ClientScheduleReview');
 
 // Client volunteer
-Route::get('/client/volunteer', [VolunteerController::class, 'ClientVolunteerList'])->name('ClientVolunteerList');
-Route::post('/client/volunteer/submit', [VolunteerController::class, 'ClientVolunteerSubmit'])->name('ClientVolunteerSubmit');
-Route::delete('/client/volunteer/delete', [VolunteerController::class, 'ClientVolunteerDelete'])->name('ClientVolunteerDelete');
-
+    Route::get('/client/volunteer', [VolunteerController::class, 'ClientVolunteerList'])->name('ClientVolunteerList');
+    Route::post('/client/volunteer/submit', [VolunteerController::class, 'ClientVolunteerSubmit'])->name('ClientVolunteerSubmit');
+    Route::delete('/client/volunteer/delete', [VolunteerController::class, 'ClientVolunteerDelete'])->name('ClientVolunteerDelete');
+});
 // Authentication
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'postLogin'])->name('postLogin');
@@ -77,45 +76,49 @@ Route::get('/register/email-otp', [RegisterController::class, 'emailOTP'])->name
 Route::post('/register/confirm-otp', [RegisterController::class, 'confirmOTP'])->name('confirmOTP');
 Route::post('/register/resend-otp', [RegisterController::class, 'resendOTP'])->name('resendOTP');
 
+Route::middleware(['access_level:0,1'])->group(function () {
 // Dashboard
-Route::get('/osca-dashboard', [DashboardController::class, 'index'])->name('dashboard');
-Route::get('/user-statistics', [AnaylticsController::class, 'getUserStatistics'])->name('getUserStatistics');
-Route::get('/services-statistics', [AnaylticsController::class, 'getServicesStatistics'])->name('getServicesStatistics');
-Route::get('/schedules-statistics', [AnaylticsController::class, 'getSchedulesStatistics'])->name('getSchedulesStatistics');
-Route::get('/top-list', [AnaylticsController::class, 'getTopList'])->name('getTopList');
-Route::get('/comorbidity-statistics', [AnaylticsController::class, 'getComorbidityStatisticxs'])->name('getComorbidityStatisticxs');
+    Route::get('/osca-dashboard', [DashboardController::class, 'index'])->name('dashboard');
+    Route::get('/user-statistics', [AnaylticsController::class, 'getUserStatistics'])->name('getUserStatistics');
+    Route::get('/services-statistics', [AnaylticsController::class, 'getServicesStatistics'])->name('getServicesStatistics');
+    Route::get('/schedules-statistics', [AnaylticsController::class, 'getSchedulesStatistics'])->name('getSchedulesStatistics');
+    Route::get('/top-list', [AnaylticsController::class, 'getTopList'])->name('getTopList');
+    Route::get('/comorbidity-statistics', [AnaylticsController::class, 'getComorbidityStatisticxs'])->name('getComorbidityStatisticxs');
 
 // User Management
-Route::get('/user-management/users/list', [UserListController::class, 'index'])->name('userList');
-Route::get('/user-list', [UserListController::class, 'getUsers'])->name('getUsers');
-Route::post('/user-management/users/add', [UserListController::class, 'store'])->name('postUser');
-Route::post('/user-management/users/{userId}', [UserListController::class, 'update'])->name('updateUser');
-Route::get('/user-management/users/view/{userId}', [UserViewController::class, 'userView'])->name('userView');
-Route::post('/user-management/users/{userId}/is-active', [UserViewController::class, 'isActive'])->name('isActive');
-Route::post('/user-management/users/{userId}/comorbidity', [UserViewController::class, 'UpdateComorbidity'])->name('UpdateComorbidity');
+    Route::get('/user-management/users/list', [UserListController::class, 'index'])->name('userList');
+    Route::get('/user-list', [UserListController::class, 'getUsers'])->name('getUsers');
+    Route::post('/user-management/users/add', [UserListController::class, 'store'])->name('postUser');
+    Route::post('/user-management/users/{userId}', [UserListController::class, 'update'])->name('updateUser');
+    Route::get('/user-management/users/view/{userId}', [UserViewController::class, 'userView'])->name('userView');
+    Route::post('/user-management/users/{userId}/is-active', [UserViewController::class, 'isActive'])->name('isActive');
+    Route::post('/user-management/users/{userId}/comorbidity', [UserViewController::class, 'UpdateComorbidity'])->name('UpdateComorbidity');
 
 // Comorbidity Management
-Route::get('/comorbidity-management/comorbidity/list', [ComorbidityController::class, 'index'])->name('comorbidityList');
-Route::get('/comorbidity-list', [ComorbidityController::class, 'getcomorbidity'])->name('getcomorbidity');
-Route::post('/comorbidity-management/comorbidity/add', [ComorbidityController::class, 'addComorbidity'])->name('addComorbidity');
-Route::post('/comorbidity-management/comorbidity/delete', [ComorbidityController::class, 'deleteComorbidity'])->name('deleteComorbidity');
-Route::post('/comorbidity-management/comorbidity/update', [ComorbidityController::class, 'updateComorbidity'])->name('updateComorbidity');
+    Route::get('/comorbidity-management/comorbidity/list', [ComorbidityController::class, 'index'])->name('comorbidityList');
+    Route::get('/comorbidity-list', [ComorbidityController::class, 'getcomorbidity'])->name('getcomorbidity');
+    Route::post('/comorbidity-management/comorbidity/add', [ComorbidityController::class, 'addComorbidity'])->name('addComorbidity');
+    Route::post('/comorbidity-management/comorbidity/delete', [ComorbidityController::class, 'deleteComorbidity'])->name('deleteComorbidity');
+    Route::post('/comorbidity-management/comorbidity/update', [ComorbidityController::class, 'updateComorbidity'])->name('updateComorbidity');
 
+});
+Route::middleware(['access_level:0,1,3'])->group(function () {
 // Profile Page
-Route::get('/profile-page', [ProfileController::class, 'profilePage'])->name('profilePage');
-Route::get('/profile-edit', [ProfileController::class, 'profileEdit'])->name('profileEdit');
-Route::post('/profile-update', [ProfileController::class, 'profileUpdate'])->name('profileUpdate');
-Route::post('/profile-comorbidty', [ProfileController::class, 'profileComorbidity'])->name('profileComorbidity');
-Route::post('/profile-email', [ProfileController::class, 'profileEmail'])->name('profileEmail');
-Route::post('/profile-password', [ProfileController::class, 'profilePassword'])->name('profilePassword');
-Route::post('/profile-username', [ProfileController::class, 'profileUsername'])->name('profileUsername');
-
+    Route::get('/profile-page', [ProfileController::class, 'profilePage'])->name('profilePage');
+    Route::get('/profile-edit', [ProfileController::class, 'profileEdit'])->name('profileEdit');
+    Route::post('/profile-update', [ProfileController::class, 'profileUpdate'])->name('profileUpdate');
+    Route::post('/profile-comorbidty', [ProfileController::class, 'profileComorbidity'])->name('profileComorbidity');
+    Route::post('/profile-email', [ProfileController::class, 'profileEmail'])->name('profileEmail');
+    Route::post('/profile-password', [ProfileController::class, 'profilePassword'])->name('profilePassword');
+    Route::post('/profile-username', [ProfileController::class, 'profileUsername'])->name('profileUsername');
+});
+Route::middleware(['access_level:0,1'])->group(function () {
 // Report Management
-Route::get('/report-management', [ReportController::class, 'index'])->name('reportList');
-Route::get('/users-report', [ReportController::class, 'usersReport'])->name('usersReport');
-Route::get('/barangay-report', [ReportController::class, 'barangayReport'])->name('barangayReport');
-Route::get('/services-report', [ReportController::class, 'servicesReport'])->name('servicesReport');
-Route::get('/comorbidities-report', [ReportController::class, 'comorbiditiesReport'])->name('comorbiditiesReport');
-Route::get('/generate-pdf', [ReportController::class, 'generatePDF'])->name('generatePDF');
-
+    Route::get('/report-management', [ReportController::class, 'index'])->name('reportList');
+    Route::get('/users-report', [ReportController::class, 'usersReport'])->name('usersReport');
+    Route::get('/barangay-report', [ReportController::class, 'barangayReport'])->name('barangayReport');
+    Route::get('/services-report', [ReportController::class, 'servicesReport'])->name('servicesReport');
+    Route::get('/comorbidities-report', [ReportController::class, 'comorbiditiesReport'])->name('comorbiditiesReport');
+    Route::get('/generate-pdf', [ReportController::class, 'generatePDF'])->name('generatePDF');
+});
 Route::get('/test-database', [DatabaseTestController::class, 'testDatabase']);
